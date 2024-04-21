c29be11b4609a07f36a894fb014cc22bf8773bd4
dharinibaskaran <143563246+dharinibaskaran@users.noreply.github.com>
2024-04-21 01:42:58 -0600

    Added Redis Cache

--- therapy_ep.py
+++ therapy_ep.py
@@ -1,12 +1,15 @@
 from flask import Flask, request, jsonify
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-from rake_nltk import Rake
+# from rake_nltk import Rake
 import requests
 from bs4 import BeautifulSoup
 from transformers import BartForConditionalGeneration, BartTokenizer
 from Bio import Entrez
+import redis
+import time
 from meshdata import mesh
+import json
 
 app = Flask(__name__)
 
@@ -16,16 +19,25 @@ app = Flask(__name__)
 
 Entrez.email = "dhba507@colorado.edu"
 
+# Connect to Redis
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+
 @app.route("/therapy", methods=["POST"])
 def therapist_endpoint():
     print("Starting to execute")
     data = request.get_json()
     user_input = data["user_input"]
     core_issue, therapy = is_therapy_needed(user_input, mesh)
-    print("YOUR ISSUE: ", core_issue )
+    print("Detected Issue: ", core_issue)
     if therapy:
         therapy_context = get_therapy_context(core_issue)
-        return jsonify({"Your Issue": core_issue, "Is Therapy Needed": True, "Therapy Context": therapy_context})
+        result = {
+            "Your Issue": core_issue, 
+            "Is Therapy Needed": True, 
+            "Therapy Context": therapy_context
+        }
+        # return jsonify({"Your Issue": core_issue, "Is Therapy Needed": True, "Therapy Context": therapy_context})
+        return result
     else:
         return jsonify({"Is Therapy Needed": "It seems therapy is not needed at the moment!"})
 
@@ -40,9 +52,7 @@ def is_therapy_needed(user_input, mesh_data):
     therapy_keywords = names
 
     therapy_keywords = [word for word in therapy_keywords if word not in stop_words]
-    new_keyword=[]
-    for word in therapy_keywords:
-        new_keyword.append(word.lower())
+    new_keyword = [word.lower() for word in therapy_keywords]
     words = word_tokenize(user_input.lower())
     for word in words:
         if word in new_keyword:
@@ -51,7 +61,7 @@ def is_therapy_needed(user_input, mesh_data):
 
 def get_therapy_context(core_issue):
     try:
-        handle = Entrez.esearch(db="pubmed", term=core_issue, retmax=3)  ##  Change retmax
+        handle = Entrez.esearch(db="pubmed", term=core_issue, retmax=3)
         search_results = Entrez.read(handle)
         handle.close()
 
@@ -60,15 +70,29 @@ def get_therapy_context(core_issue):
 
         for article_id in article_ids:
             article_info = {}
+
+            start_time = time.time() 
+
             summary_handle = Entrez.esummary(db="pubmed", id=article_id)
             summary_result = Entrez.read(summary_handle)
             summary_handle.close()
 
+            summary = redis_client.get(article_id)
             article_info['title'] = summary_result[0]['Title']
             article_info['url'] = f"https://pubmed.ncbi.nlm.nih.gov/{article_id}/"
             article_info['pmid'] = article_id
-            article_info['summary'] = scrape_content(article_info['url'])
+
+            if summary:
+                get_result = summary.decode("utf-8")
+                article_info['summary'] = get_result
+            else:
+                article_info['summary'] = scrape_content(article_info['url'])
+                redis_client.set(article_id, article_info['summary'])
+            end_time = time.time()
+
+            print(f"Time taken to fetch summary for article {article_id}: {end_time - start_time} seconds")
             articles.append(article_info)
+            
 
         therapy_context = []
         for article_info in articles:
@@ -80,12 +104,12 @@ def get_therapy_context(core_issue):
                     "Article Summary": article_info['summary']
                 }
             }
-            therapy_context.append(context)
-        
+            therapy_context.append(context)       
+
     except Exception as e:
         print("Error occurred during NLM search:", e)
         return {"recommendations": ["No recommendations available due to an error"]}
-    
+
     return therapy_context
 
 def scrape_content(url):
